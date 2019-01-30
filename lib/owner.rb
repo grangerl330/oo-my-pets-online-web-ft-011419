858e7f8c7e45658d189d65e0665031b35803a4a0
Luke Granger <grangerl330@gmail.com>
2019-01-30 01:38:31 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -18,6 +18,14 @@ class Owner
     @pets[:fishes] << Fish.new(name)
   end 
   
+  def buy_cat(name)
+    @pets[:cats] << Cat.new(name)
+  end 
+  
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
+  end 
+  
   def self.all
     @@all 
   end 
